e11923320a04f2ae0bd589c528a54a0bb1bf4a6d
ShauryaThakur01 <shauryathakur2004@gmail.com>
2023-09-06 14:39:52 +0530

    second commit

--- src/constants/data.js
+++ src/constants/data.js
@@ -2,56 +2,56 @@ import images from './images';
 
 const wines = [
   {
-    title: 'Chapel Hill Shiraz',
-    price: '$56',
-    tags: 'AU | Bottle',
+    title: 'Curry Fried Rice',
+    price: '₹100',
+    tags: 'Full plate | Half plate',
   },
   {
-    title: 'Catena Malbee',
-    price: '$59',
-    tags: 'AU | Bottle',
+    title: 'Aloo paratha',
+    price: '₹50',
+    tags: 'Full plate | Half plate',
   },
   {
-    title: 'La Vieillw Rose',
-    price: '$44',
-    tags: 'FR | 750 ml',
+    title: 'Idli Dosa Sambhar',
+    price: '₹60',
+    tags: 'Full plate | Half plate',
   },
   {
-    title: 'Rhino Pale Ale',
-    price: '$31',
-    tags: 'CA | 750 ml',
+    title: 'Rice and Dal',
+    price: '₹100',
+    tags: 'Full plate | Half plate',
   },
   {
-    title: 'Irish Guinness',
-    price: '$26',
-    tags: 'IE | 750 ml',
+    title: 'Makki ki Roti and Sarson ka Sag',
+    price: '₹100',
+    tags: 'Full plate | Half plate',
   },
 ];
 
 const cocktails = [
   {
-    title: 'Aperol Sprtiz',
-    price: '$20',
+    title: 'Onion Pakoda',
+    price: '₹20',
     tags: 'Aperol | Villa Marchesi prosecco | soda | 30 ml',
   },
   {
-    title: "Dark 'N' Stormy",
-    price: '$16',
+    title: "Veg Cutlet",
+    price: '₹16',
     tags: 'Dark rum | Ginger beer | Slice of lime',
   },
   {
-    title: 'Daiquiri',
-    price: '$10',
+    title: 'Aloo Tikki',
+    price: '₹10',
     tags: 'Rum | Citrus juice | Sugar',
   },
   {
-    title: 'Old Fashioned',
-    price: '$31',
+    title: 'French Fries',
+    price: '₹31',
     tags: 'Bourbon | Brown sugar | Angostura Bitters',
   },
   {
-    title: 'Negroni',
-    price: '$26',
+    title: 'Pav Bhaji',
+    price: '₹26',
     tags: 'Gin | Sweet Vermouth | Campari | Orange garnish',
   },
 ];
